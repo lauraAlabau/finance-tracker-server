e8f3e0f1a53a51e5c8db8a1c6356cd2ea7fa08d9
lauraAlabau <arlaura3@gmail.com>
2024-06-11 18:06:21 +0200

    add index

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Router } from "express";
+import mongoose from "mongoose";
+import financeRecordRouter from "./src/routes/finance-record";
+import cors from "cors";
+import config from "./config";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const URI: string = config.DB_REMOTE;
+
+mongoose
+  .connect(URI)
+  .then(() => console.log("CONNECTED SUCCESS"))
+  .catch((err) => console.error("FAIL CONNECTION", err));
+
+app.use("/finances-record", financeRecordRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server Runing on Port ${PORT}`);
+});
